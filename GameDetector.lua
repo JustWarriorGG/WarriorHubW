a21d801653a5d32f53e66e6c1831b18e54f529ca
JustWarriorGG <136884544+JustWarriorGG@users.noreply.github.com>
2024-01-25 16:23:17 +0300

    Create GameDetector.lua

---
+++ GameDetector.lua
@@ -0,0 +1,8 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 13772394625 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustWarriorGG/WarriorHubW/main/bladeball.lua")()
+elseif PlaceId == 8260276694 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JustWarriorGG/WarriorHubW/main/abilitywars.lua")()
+elseif PlaceId == 537413528 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JustWarriorGG/WarriorHubW/main/babft.lua")()
